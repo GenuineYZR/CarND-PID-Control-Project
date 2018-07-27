6fc75417971f6811a20e17bd745f58d7d7f3534c
GenuineYZR <18260632926@163.com>
2018-07-27 22:50:50 +0800

    Initialize the pid variable & Calculate steering value

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,10 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
 
+  pid.i_error = 0;
+
+  pid.Init(0.1, 0.0004, 3);
+
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -58,6 +62,10 @@ int main()
           * another PID controller to control the speed!
           */
           
+		  pid.UpdateError(cte);
+
+		  steer_value = pid.TotalError() / deg2rad(25);
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
